71a736482b3cba71f6552ffd34206b1109e365e4
YurchenkoEV <YurchenkoEV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-14 05:18:55 +0000

    Transliteration plugin
    git-svn-id: https://plugins.svn.wordpress.org/wp-translitera/trunk@1198508 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== Plugin Name ===
+Contributors: YurchenkoEV
+Donate link: http://yur4enko.com/
+Tags: spam, mail, protection
+Requires at least: 1.2
+Tested up to: 4.2.2
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Transliteration plugin
+
+== Description ==
+
+Transliteration plugin
+
+== Installation ==
+
+Automatical from repository or unzip in plugins and activate in ACP.
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+== Changelog ==
+all info:
+http://azbuki.info/viewforum.php?f=32
+
+== Upgrade Notice ==
+After update deactivate and activate plugin
+
+== Arbitrary section ==
+--
+
+== A brief Markdown Example ==
+
+`<?php code(); // goes in backticks ?>`
---
+++ wp-translitera.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Plugin Name: WP Translitera
+Plugin URI: http://azbuki.info/viewforum.php?f=32
+Description: Protection from spam through your blog
+Version: 150712
+Author: Evgen Yurchenko
+Author URI: http://yur4enko.com/
+*/
+
+/*  Copyright 2015 Evgen Yurchenko  (email: evgen@yur4enko.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc.
+*/
+
+class wp_translitera {
+    public static function transliterate($title) {
+        $to = array(
+            'А'=>'A','а'=>'a','Б'=>'B','б'=>'b','В'=>'V','в'=>'v','Г'=>'G',
+            'г'=>'g','Д'=>'D','д'=>'d','Е'=>'E','е'=>'e','Ё'=>'Jo','ё'=>'jo',
+            'Ж'=>'Zh','ж'=>'zh','З'=>'Z','з'=>'z','И'=>'I','и'=>'i','Й'=>'J',
+            'й'=>'j','К'=>'K','к'=>'k','Л'=>'L','л'=>'l','М'=>'M','м'=>'m',
+            'Н'=>'N','н'=>'n','О'=>'O','о'=>'o','П'=>'P','п'=>'p','Р'=>'R',
+            'р'=>'r','С'=>'S','с'=>'s','Т'=>'T','т'=>'t','У'=>'U','у'=>'u',
+            'Ф'=>'F','ф'=>'f','Х'=>'H','х'=>'h','Ц'=>'C','ц'=>'c','Ч'=>'Ch',
+            'ч'=>'ch','Ш'=>'Sh','ш'=>'sh','Щ'=>'Shh','щ'=>'shh','Ъ'=>'',
+            'ъ'=>'','Ы'=>'Y','ы'=>'y','Ь'=>'','ь'=>'','Э'=>'Je','э'=>'je',
+            'Ю'=>'Ju','ю'=>'ju','Я'=>'Ja','я'=>'ja'
+        );
+        return strtr($title, $to);
+    }
+}
+
+add_action('sanitize_title', array('wp_translitera','transliterate'), 0);
